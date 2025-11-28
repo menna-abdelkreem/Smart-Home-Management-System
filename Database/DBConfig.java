7e5774f735a42ec4ee4a7ed55ff1ccb581afed4a
Ola-ahmed52 <ola01019588752@gmail.com>
2025-11-28 14:02:33 +0200

    Add DBConfig class for database connection management

--- Database/DBConfig.java
+++ Database/DBConfig.java
@@ -1,1 +1,25 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConfig {
+
+    private static final String URL =
+            "jdbc:sqlserver://localhost:1433;databaseName=SmartHome;encrypt=false;";
+
+    private static final String USER = "javauser";   // SQL User
+    private static final String PASS = "123456";     // SQL Password
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Connected Successfully!");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
